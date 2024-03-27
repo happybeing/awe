62f880d9bc336457ab611c072193d752bf4e0089
Mark <git@happybeing.com>
2024-03-27 21:34:49 +0000

    Tidy up GUI layout by shifting features from components to main page

--- src/lib/Navigation.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-function handleBack() {}
-
-function handleForward() {}
-
-</script>
-
-<button on:click={handleBack}>&lt;</button>
-<button on:click={handleForward}>&gt;</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,73 @@
 <script>
 import logo from '../images/icon.png';
-import AddressBar from '../lib/AddressBar.svelte'
-import Navigation from '../lib/Navigation.svelte'
-import WebsiteView from '../lib/WebsiteView.svelte'
 
-import { webAddressStore } from '$lib/stores.js'
+let webViewTitle="Autonomi Browser Webview";
+let webViewContent = "<HTML><HEAD></HEAD><BODY><h1>Default</h1> content</BODY></HTML>";
+let webViewUrl = '';
 
-//$: webViewContent = "<HTML><HEAD></HEAD><BODY><h1>Main page</h1>content is address: " + $webAddressStore + "</BODY></HTML>";
-let webViewContent = "EMPTY";
+let rowNavigationHeight = "20px";
 
-let webViewUrl = 'http://autonomi.com';
-$: webViewUrl = $webAddressStore;
+function handleBack() {}
+function handleForward() {}
 
+let val = 'http://autonomi.com';
+
+function handleSubmit() {
+  webViewUrl = val;
+}
 </script>
 
-<div style="display:block; float:left;">
-    <div>
-        <Navigation/>
-        <AddressBar/>
-    </div>
+<style>
+.container {
+  height: vh;
+  width: 100%;
+  white-space: nowrap;
+  overflow: hidden;
+}
+
+.button {
+  height: 4ch;
+  width: 4ch;
+}
+.button,
+.input-form {
+  display: inline-block;
+  height: 32px;
+  margin-top: 4px;
+  margin-bottom: 4px;
+  margin-left: 4px;
+  line-height: 3ch;
+}
+
+.input-form {
+  position: relative;
+}
+
+input[type="text"] {
+  height: 34px;
+  font-size: 1.2em;
+  padding-left: 8px;
+  margin-left: 8px;
+}
+
+.iframe-wrapper iframe,
+.iframe-wrapper object,
+.iframe-wrapper embed {
+  height: calc(100vh - 50px); /* Prevent vertical scrolling of everything inside body */
+  width: 100%;
+  border: 0;
+}
+
+</style>
+
+<div class="container">
+    <button class="button" onclick="button1Function()">&lt;</button>
+    <button class="button" onclick="button2Function()">&gt;</button>
+<form class="input-form" on:submit|preventDefault={handleSubmit}>
+  Type an xor:// or http:// URL and press enter:<input class="input" type=text bind:value={val} placeholder="autonomi address" />
+</form>
 </div>
-<WebsiteView {webViewContent} {webViewUrl}/>
 
-
+<div class="row-view iframe-wrapper" >
+  <iframe src={webViewUrl} name="webview" title={webViewTitle}></iframe>
+</div>
